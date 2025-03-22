534918b95e5991efecea41adf99246ce454d40aa
Billynill <avezzzov_r@mail.ru>
2025-03-23 01:20:52 +0300

    Исправил работу карты и адресов

---
+++ PizzaFlow/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  PizzaFlow
+//
+//  Created by 596 on 23.03.2025.
+//
+
+import SwiftUI
+
+struct LocationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationManager()
+}
---
+++ PizzaFlow/Models/AdressModel.swift
@@ -0,0 +1,8 @@
+//
+//  AdressModel.swift
+//  PizzaFlow
+//
+//  Created by 596 on 23.03.2025.
+//
+
+import Foundation
